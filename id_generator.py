afa8da8772bc012ec55a56dd4dd9c699cb517a4d
M3T1X1 <dusza-kacper@wp.pl>
2024-08-04 16:56:35 +0200

    sorry wrong folder for the python scripts

--- id_generator.py
+++
@@ -1,5 +0,0 @@
-import string
-import random
-
-id = ''.join(random.choice('QWERTYUIOPASDFGHJKLZXCVBNM1234567890') for i in range(11))
-print('ID:',id)
--- vin_generator.py
+++
@@ -1,5 +0,0 @@
-import string
-import random
-
-vin = ''.join(random.choice('WERTYUIOPASDFGHJKLZXCVBNM1234567890') for i in range(17))
-print('VIN: ',vin)
